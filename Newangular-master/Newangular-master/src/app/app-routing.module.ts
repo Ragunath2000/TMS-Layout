b6dd0f8867f729c8e77371c9dd63ddccfb03a1ba
Sriram1745 <102479268+Ragunath2000@users.noreply.github.com>
2022-05-03 23:29:14 +0530

    angular

---
+++ Newangular-master/Newangular-master/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AssignedreviewComponent } from './assignedreview/assignedreview.component';
+import { CancelReviewComponent } from './cancel-review/cancel-review.component';
+import { CompletedReviewComponent } from './completed-review/completed-review.component';
+import { EditreviewComponent } from './editreview/editreview.component';
+import { MyreviewComponent } from './myreview/myreview.component';
+import { ScheduledreviewComponent } from './scheduledreview/scheduledreview.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  {path:'scheduledreview',component:ScheduledreviewComponent},
+  {path:'myreview',component:MyreviewComponent},
+  {path:'editreview',component:EditreviewComponent},
+  {path:'completed-review',component:CompletedReviewComponent},
+  {path:'cancel-review',component:CancelReviewComponent},
+  {path:'assignedreview',component:AssignedreviewComponent},
+  {path:'navbar',component:NavbarComponent}
+
+
+
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Newangular-master/Newangular-master/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AssignedreviewComponent } from './assignedreview/assignedreview.component';
+import { CompletedReviewComponent } from './completed-review/completed-review.component';
+import { MyreviewComponent } from './myreview/myreview.component';
+import { CancelReviewComponent } from './cancel-review/cancel-review.component';
+import { EditreviewComponent } from './editreview/editreview.component';
+import { ScheduledreviewComponent } from './scheduledreview/scheduledreview.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AssignedreviewComponent,
+    CompletedReviewComponent,
+    MyreviewComponent,
+    CancelReviewComponent,
+    EditreviewComponent,
+    ScheduledreviewComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
